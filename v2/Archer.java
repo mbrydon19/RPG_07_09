db9244539ee3a05d61be1eb177625f264797fcd5
Jason Cheung <34114652+jcheung7@users.noreply.github.com>
2017-12-05 22:26:35 -0500

    Update Archer.java

--- v2/Archer.java
+++ v2/Archer.java
@@ -1,11 +1,7 @@
 public class Archer extends Protagonist {
-<<<<<<< HEAD
-    public Archer() {
-	super(name);
-=======
+
     public Archer(String pName) {
 	super(pName);
->>>>>>> 6180efe140c6cb4adbf463d909b24c1419691975
         strength = 60;
     }
     public String toString() {
