93b020dc947ebcca71cd5d5bdb0fed637f1619c0
Jason Cheung <34114652+jcheung7@users.noreply.github.com>
2017-12-05 22:27:36 -0500

    Update Warrior.java

--- v2/Warrior.java
+++ v2/Warrior.java
@@ -1,11 +1,10 @@
 public class Warrior extends Protagonist {
-<<<<<<< HEAD
+	
     public Warrior() {
 	super(name);
-=======
+
     public Warrior(String pName) {
 	super(pName);
->>>>>>> 6180efe140c6cb4adbf463d909b24c1419691975
         defense = 30;
     }
     public String toString() {
